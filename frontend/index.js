01a58dc205c3dc3deea7f9652e7f4e969a9293bf
Chase Darnall <emailchased@gmail.com>
2023-11-07 14:16:41 -0600

    done

--- frontend/index.js
+++ frontend/index.js
@@ -3,8 +3,17 @@ function moduleProject3() {
   // 👉 TASK 1 - Write a `buildNav` component that returns a nav
 
   function buildNav(links) {
-    //  ✨ do your magic here
-    return document.createElement('nav')
+    const navigate = document.createElement('nav');
+    links.forEach(link => {
+    const aLink = document.createElement('a');
+    aLink.href = link.href;
+    aLink.title = link.title;
+    aLink.textContent = link.textContent;
+   //document.body.appendChild(navigate);
+    navigate.appendChild(aLink);
+
+    })
+    return navigate
   }
 
   // ❗ DOM creation using your `buildNav` component (do not change):
@@ -19,7 +28,32 @@ function moduleProject3() {
   // 👉 TASK 2A - Write a `buildLearnerCard` component that returns a card
 
   function buildLearnerCard(learner, languages) {
-    //  ✨ do your magic here
+     const cardDiv = document.createElement('div');
+     cardDiv.classList.add('learner-card');
+     //cardDiv.textContent = 'WIP';
+     const para1 = document.createElement('p');
+     para1.textContent = learner.fullName
+     const para2 = document.createElement('p');
+     para2.textContent = `Learner ID: ${learner.id}`
+     const para3 = document.createElement('p');
+     para3.textContent = ` Date of Birth: ${learner.dateOfBirth}`;
+     const para4 = document.createElement('p');
+     const favLang = languages.find(lang => lang.id === learner.favLanguage);
+     para4.textContent = `Favorite Language: ${favLang.name}`;
+    cardDiv.appendChild(para1)
+    cardDiv.appendChild(para2)
+    cardDiv.appendChild(para3)
+    cardDiv.appendChild(para4)
+    cardDiv.addEventListener('click', evt => {
+     document.querySelectorAll('.learner-card').forEach(card => {
+      card.classList.remove('active')
+      })
+      cardDiv.classList.add('active')
+    } )
+
+     
+     return cardDiv;
+
   }
 
   {
@@ -39,14 +73,54 @@ function moduleProject3() {
       { id: 41, fullName: 'Sabah Beydoun', dateOfBirth: '1988-03-25', favLanguage: 91 },
       { id: 17, fullName: 'Daniel Castillo', dateOfBirth: '1995-11-05', favLanguage: 12 }
     ]
-    //  ✨ do your magic here
+    learners.forEach(learner => {
+    const card = buildLearnerCard(learner, languages);
+    document.querySelector('section').appendChild(card)
+    })
   }
 
   // 👉 TASK 3 - Write a `buildFooter` component that returns a footer
 
   function buildFooter(footerData) {
-    //  ✨ do your magic here
-    return document.createElement('footer')
+    const footer = document.createElement('footer')
+    const div1 = document.createElement('div')
+    div1.classList.add('company-info')
+    const div2 = document.createElement('div')
+    div2.classList.add('social-media')
+    const d1p1 = document.createElement('p')
+    const d1p2 = document.createElement('p')
+    const d1p3 = document.createElement('p')
+    const d1p3A = document.createElement('a')
+    d1p1.textContent = footerData.companyName
+    d1p1.classList.add('company-name')
+    d1p2.textContent = footerData.address
+    d1p2.classList.add('address')
+    d1p3A.href = 'mailto:' + footerData.contactEmail
+    d1p3A.textContent = footerData.contactEmail
+    d1p3.classList.add('contact-email')
+    const d2p1 = document.createElement('a')
+    const d2p2 = document.createElement('a')
+    const d2p3 = document.createElement('a')
+    d2p1.href = footerData.socialMedia.twitter;
+    d2p1.textContent = 'Twitter'
+    d2p2.href = footerData.socialMedia.facebook;
+    d2p2.textContent = 'Facebook'
+    d2p3.href = footerData.socialMedia.instagram;
+    d2p3.textContent = 'Instagram'
+    const div3 = document.createElement('div')
+    div3.textContent = '© BLOOM INSTITUTE OF TECHNOLOGY 2023'
+    footer.appendChild(div1)
+    footer.appendChild(div2)
+    footer.appendChild(div3)
+    div1.appendChild(d1p1)
+    div1.appendChild(d1p2)
+    div1.appendChild(d1p3)
+    div2.appendChild(d2p1)
+    div2.appendChild(d2p2)
+    div2.appendChild(d2p3)
+    d1p3.appendChild(d1p3A)
+
+    return footer
   }
 
   // ❗ DOM creation using your `buildFooter` component (do not change):
@@ -59,6 +133,8 @@ function moduleProject3() {
       facebook: 'https://www.facebook.com/example',
       instagram: 'https://www.instagram.com/example',
     },
+    
+
   }))
 
   // 👉 TASK 4 - Clicking on the section should deactivate the active card
